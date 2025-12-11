e965ec51759289e01d182bf27200401a973a0840
Ben Rees <ben@rees.biz>
2025-12-11 15:56:41 +1300

    Change approach to only update data and not create a chart each time

--- Tasks/ExportEngineeringTaskAnalysis.cs
+++ Tasks/ExportEngineeringTaskAnalysis.cs
@@ -6,7 +6,7 @@ namespace BensEngineeringMetrics.Tasks;
 /// <summary>
 ///     See https://javlnsupport.atlassian.net/wiki/spaces/DEVELOPMEN/pages/1284243457/Engineering+Tasks+Summary+-+November+2025
 /// </summary>
-public class ExportEngineeringTaskAnalysis(IJiraQueryRunner runner, IWorkSheetUpdater sheetUpdater, ICsvExporter exporter, ISheetPieChart piecharter) : IEngineeringMetricsTask
+public class ExportEngineeringTaskAnalysis(IJiraQueryRunner runner, IWorkSheetUpdater sheetUpdater, ICsvExporter exporter) : IEngineeringMetricsTask
 {
     private const string GoogleSheetId = "1_ANmhfs-kjyCwntbeS2lM0YYOpeBgCDoasZz5UZpl2g";
     private const string TaskKey = "ENG_TASK_ANALYSIS";
@@ -37,20 +37,12 @@ public class ExportEngineeringTaskAnalysis(IJiraQueryRunner runner, IWorkSheetUp
         Console.WriteLine($"{Key} - {Description}");
         Console.WriteLine();
 
-        // Delete tab to clear previous data
         await sheetUpdater.Open(GoogleSheetId);
-        sheetUpdater.DeleteSheet(PiechartSheetTab);
-        sheetUpdater.AddSheet(PiechartSheetTab);
-        await sheetUpdater.SubmitBatch();
 
-        await sheetUpdater.Open(GoogleSheetId);
-        await piecharter.Open(GoogleSheetId, sheetUpdater);
-
-        await CreatePieChart();
         await CreateMonthTicketSheet();
+        CreatePieChartData();
 
         await sheetUpdater.SubmitBatch();
-        await piecharter.SubmitBatch();
     }
 
     private async Task CreateMonthTicketSheet()
@@ -79,25 +71,72 @@ public class ExportEngineeringTaskAnalysis(IJiraQueryRunner runner, IWorkSheetUp
         await sheetUpdater.ImportFile($"{sheetName}!A1", fileName);
     }
 
-    private async Task CreatePieChart()
+    private void CreatePieChartData()
     {
-        sheetUpdater.ClearRange($"{PiechartSheetTab}");
+        sheetUpdater.ClearRange($"{PiechartSheetTab}", "A1:D100");
 
         var chartData = new List<IList<object?>>
         {
-            new List<object?> { "Category", "Percentage" }, // Header
-            new List<object?> { "Bug", 28.4 },
-            new List<object?> { "Improvement", 8.7 },
-            new List<object?> { "Story", 60.5 },
-            new List<object?> { "Other", 1.0 }
+            new List<object?> { "Date Range", "1-Nov-25", "1-Dec-25" },
         };
+        sheetUpdater.EditSheet($"'{PiechartSheetTab}'!A1", chartData, true);
 
-        await piecharter.InsertPieChart(
-            $"'{PiechartSheetTab}'!A1",
-            chartData,
-            0, 0,
-            "Engineering Task Types - Last Month");
+        // var chartData = new List<IList<object?>>
+        // {
+        //     new List<object?> { "Date Range", "1-Nov-25", "1-Dec-25" },
+        //     new List<object?> { "Ticket Type", "Percentage", "Count" },
+        //     new List<object?> { "Story", 53.7/100, 165 },
+        //     new List<object?> { "Bug", 31.9/100, 98 },
+        //     new List<object?> { "Improvement", 9.8/100, 30 },
+        //     new List<object?> { "Sub-Task", 1.6/100, 5 },
+        //     new List<object?> { "Customer Change Request", 1.0/100, 3 },
+        //     new List<object?> { "Functional Requirement", 1.0/100, 3 },
+        //     new List<object?> { "Non-Functional Requirement", 0.7/100, 2 },
+        //     new List<object?> { "Schema Task", 0.3/100, 1 },
+        //     new List<object?> { "Total", null, 307 }
+        // };
+
+        var totalCount = (double)this.issues.Count;
+        if (totalCount == 0)
+        {
+            Console.WriteLine("No data returned.");
+            return;
+        }
+
+        var totalPoints = this.issues.Sum(i => i.StoryPoints == 0 ? 1 : i.StoryPoints);
+        var data = this.issues
+            .GroupBy(i => i.IssueType)
+            .Select<IGrouping<string, JiraIssue>, EngineeringTicketTypeChart>(g =>
+                new EngineeringTicketTypeChart(
+                    g.Key,
+                    g.Count() / totalCount,
+                    g.Count(),
+                    g.Sum(i => i.StoryPoints == 0 ? 1 : i.StoryPoints) / totalPoints
+                ))
+            .OrderByDescending(g => g.Count)
+            .ToList();
+
+        chartData = [new List<object?> { "Ticket Type", "Percentage", "Ticket Count", "Story Points Percentage" }];
+
+        foreach (var group in data)
+        {
+            chartData.Add(new List<object?>
+            {
+                group.TicketType,
+                group.Percentage,
+                group.Count,
+                group.StoryPointsPercentage,
+            });
+        }
+
+        chartData.Add(new List<object?> { "Total", null, totalCount, totalPoints });
+
+        sheetUpdater.EditSheet($"'{PiechartSheetTab}'!A2", chartData, true);
+        sheetUpdater.BoldCellsFormat(PiechartSheetTab, 1, 2, 0, 4);
+        sheetUpdater.BoldCellsFormat(PiechartSheetTab, chartData.Count-1, chartData.Count, 0, 4);
     }
+
+    private record EngineeringTicketTypeChart(string TicketType, double Percentage, int Count, double StoryPointsPercentage);
 
     private record JiraIssue(
         string Key,
