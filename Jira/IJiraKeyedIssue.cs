5e8377467c555e5566333d6dc5e3a717ba95b114
Ben Rees <ben@rees.biz>
2025-12-15 13:59:53 +1300

    Add doc

--- Jira/IJiraKeyedIssue.cs
+++ Jira/IJiraKeyedIssue.cs
@@ -1,5 +1,9 @@
 namespace BensEngineeringMetrics.Jira;
 
+/// <summary>
+///     An interface used primarily with Basic Jira Types <see cref="BasicJiraInitiative" /> <see cref="BasicJiraPmPlan" /> <see cref="BasicJiraTicket" />.
+///     Simply represents any ticket by surfacing only its key and type.
+/// </summary>
 public interface IJiraKeyedIssue
 {
     string IssueType { get; }
--- Jira/IJsonToJiraBasicTypeMapper.cs
+++ Jira/IJsonToJiraBasicTypeMapper.cs
@@ -3,6 +3,9 @@ using System.Text.Json.Nodes;
 
 namespace BensEngineeringMetrics.Jira;
 
+/// <summary>
+///     A mapper class to encapsulate all JSON to Jira type mapper functions. These are primarily used when its not ideal to use the <see cref="JiraQueryDynamicRunner" />
+/// </summary>
 internal interface IJsonToJiraBasicTypeMapper
 {
     AgileSprint CreateAgileSprintFromJsonNode(JsonNode? json);
