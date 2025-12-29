00004f382f0f89759af0ae0bd8f8f13a5e4023be
Ben Rees <ben@rees.biz>
2025-12-29 17:35:32 +1300

    Update Weekly.bat to not be folder specific

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -1,11 +1,10 @@
 cls
 @ECHO OFF
 ECHO Start time: %date% %time%
-cd \Development\BensEngineeringMetrics
 git checkout master
 git pull
 dotnet build BensEngineeringMetrics.sln
-cd \Development\BensEngineeringMetrics\Bin\Debug\net9.0
+cd Bin\Debug\net9.0
 BensEngineeringMetricsConsole.exe BUG_STATS
 BensEngineeringMetricsConsole.exe INCIDENTS
 BensEngineeringMetricsConsole.exe INIT_ALL
