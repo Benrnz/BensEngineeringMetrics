3f7bd55a62bd7ddf6b1b087d70aeffebd1e0d82d
Ben Rees <ben@rees.biz>
2026-01-20 16:10:40 +1300

    Output added to validate with log files

--- BensEngineeringMetrics.Test/CalculatePmPlanReleaseBurnUpValuesTest.cs
+++ BensEngineeringMetrics.Test/CalculatePmPlanReleaseBurnUpValuesTest.cs
@@ -8,8 +8,8 @@ namespace BensEngineeringMetrics.Test;
 
 public class CalculatePmPlanReleaseBurnUpValuesTest
 {
-    [Fact(Skip = "Only run manually")]
-    //[Fact]
+    //[Fact(Skip = "Only run manually")]
+    [Fact]
     public async Task Recorder()
     {
         var mapper = new JsonToJiraBasicTypeMapper();
@@ -36,7 +36,17 @@ public class CalculatePmPlanReleaseBurnUpValuesTest
         var sut = new CalculatePmPlanReleaseBurnUpValues(mockExporter, envestPmPlanStories);
 
         await sut.ExecuteAsync(["TASKID"]);
-        Debugger.Break();
+
+        /*
+As at 20/01/2026
+Total work to be done: 1733.05
+Work completed: 1486.05
+PmPlans with High level estimates only: 28
+PmPlans with no estimate: 11
+PmPlans with Spec'ed and Estimated: 24
+Stories with no estimate: 167 / 292
+Average Velocity (last 6 weeks): 29.7 story points per sprint
+         */
         Assert.Fail();
     }
 }
