3d4b76378b2e44a324ef43da52f4c4ed32b05c32
Ben Rees <ben@rees.biz>
2026-01-22 12:48:12 +1300

    Correct path of console.exe in Weeklybat

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -5,12 +5,12 @@ git checkout master
 git pull
 dotnet build BensEngineeringMetrics.sln
 cd BensEngineeringMetrics.Console\Bin\Debug\net9.0
-BensEngineeringMetricsConsole.exe BUG_STATS
-BensEngineeringMetricsConsole.exe INCIDENTS
-BensEngineeringMetricsConsole.exe INIT_ALL
-BensEngineeringMetricsConsole.exe SPRINT_PLAN
-BensEngineeringMetricsConsole.exe ENG_TASK_ANALYSIS
-BensEngineeringMetricsConsole.exe ENVEST_EXALATE
+BensEngineeringMetrics.Console.exe BUG_STATS
+BensEngineeringMetrics.Console.exe INCIDENTS
+BensEngineeringMetrics.Console.exe INIT_ALL
+BensEngineeringMetrics.Console.exe SPRINT_PLAN
+BensEngineeringMetrics.Console.exe ENG_TASK_ANALYSIS
+BensEngineeringMetrics.Console.exe ENVEST_EXALATE
 
 ECHO Finish time: %date% %time%
 
