45cc57a59587e2a3735f1aece4e73f807fef898d
Ben Rees <ben@rees.biz>
2026-02-11 09:33:34 +1300

    Remove unused task for Calculating overall Envest burn down

--- BensEngineeringMetrics.Console/Jira/JiraTeamConfig.cs
+++ BensEngineeringMetrics.Console/Jira/JiraTeamConfig.cs
@@ -6,10 +6,11 @@ public static class JiraConfig
 {
     public static readonly TeamConfig[] Teams =
     [
-        new("Superclass", Constants.TeamSuperclass, 419, 60, Constants.JavPmJiraProjectKey),
-        new("RubyDucks", Constants.TeamRubyDucks, 420, 60, Constants.JavPmJiraProjectKey),
-        new("Spearhead", Constants.TeamSpearhead, 418, 60, Constants.JavPmJiraProjectKey),
-        new("Officetech", Constants.TeamOfficetech, 483, 35, Constants.OtPmJiraProjectKey),
-        new("Integration", Constants.TeamIntegration, 450, 40, Constants.JavPmJiraProjectKey)
+        new("Superclass", Constants.TeamSuperclass, 419, 40, Constants.JavPmJiraProjectKey),
+        new("Phantom", Constants.TeamPhantom, 1176, 10, Constants.JavPmJiraProjectKey),
+        new("RubyDucks", Constants.TeamRubyDucks, 420, 50, Constants.JavPmJiraProjectKey),
+        new("Spearhead", Constants.TeamSpearhead, 418, 70, Constants.JavPmJiraProjectKey),
+        new("Officetech", Constants.TeamOfficetech, 483, 45, Constants.OtPmJiraProjectKey),
+        new("Integration", Constants.TeamIntegration, 450, 50, Constants.JavPmJiraProjectKey)
     ];
 }
--- BensEngineeringMetrics.Console/Program.cs
+++ BensEngineeringMetrics.Console/Program.cs
@@ -27,7 +27,7 @@ public static class Program
             services.AddTransient<ISheetPieChart, GooglePieChart>();
             services.AddSingleton<IJiraIssueRepository, JiraIssueRepository>();
             services.AddSingleton<IJsonToJiraBasicTypeMapper, JsonToJiraBasicTypeMapper>();
-            services.AddSingleton<IEnvestPmPlanStories, EnvestPmPlanStories>();
+            //services.AddSingleton<IEnvestPmPlanStories, EnvestPmPlanStories>();
             services.AddTransient<IApiClientFactory, JiraApiClientFactory>();
             services.AddTransient<IOutputter, ConsoleOutputter>();
 
