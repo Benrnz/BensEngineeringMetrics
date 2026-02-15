4fb91994c577575e74e7fcf66af5f7a585105260
Ben Rees <ben@rees.biz>
2026-02-16 12:56:24 +1300

    Include zero estimate ticket numbers in the daily sprint report

--- BensEngineeringMetrics.Console/Tasks/CalculateDailyReportTask.cs
+++ BensEngineeringMetrics.Console/Tasks/CalculateDailyReportTask.cs
@@ -1,4 +1,5 @@
-﻿using BensEngineeringMetrics.Jira;
+﻿using System.Text;
+using BensEngineeringMetrics.Jira;
 
 namespace BensEngineeringMetrics.Tasks;
 
@@ -77,13 +78,24 @@ public class CalculateDailyReportTask(ICsvExporter exporter, IJiraQueryRunner ru
         var p1Bugs = tickets.Count(t => t.Type == Constants.BugType && t is { Severity: Constants.SeverityCritical, BugType: Constants.BugTypeProduction or Constants.BugTypeUat });
         var p2Bugs = tickets.Count(t => t.Type == Constants.BugType && t is { Severity: Constants.SeverityMajor, BugType: Constants.BugTypeProduction or Constants.BugTypeUat });
         var ticketNoEstimate = tickets.Count(t => t.Type is Constants.StoryType or Constants.BugType or Constants.ImprovementType or Constants.SchemaTaskType && t.StoryPoints == 0);
+        var zeroEstimateTickets = new StringBuilder();
+        if (ticketNoEstimate > 0)
+        {
+            zeroEstimateTickets.Append("(");
+            foreach (var ticket in tickets.Where(t => t.Type is Constants.StoryType or Constants.BugType or Constants.ImprovementType or Constants.SchemaTaskType && t.StoryPoints == 0))
+            {
+                zeroEstimateTickets.Append($"{ticket.Key}, ");
+            }
+            zeroEstimateTickets.Append(").");
+        }
+
         Console.WriteLine($"{teamName} Team Stats:");
         Console.WriteLine($"     - Total Tickets: {totalTickets}, {remainingTickets} remaining, {totalTickets - remainingTickets} done. ({1 - ((double)remainingTickets / totalTickets):P0} Done). ");
         Console.WriteLine(
             $"     - Total Story Points: {totalStoryPoints}, {remainingStoryPoints} remaining, {totalStoryPoints - remainingStoryPoints:F1} done. ({1 - (remainingStoryPoints / totalStoryPoints):P0} Done).");
         Console.WriteLine($"     - In Dev: {ticketsInDev}, In QA: {ticketsInQa}");
         Console.WriteLine($"     - Number of Flags raised: {ticketsFlagged}");
-        Console.WriteLine($"     - Number of sprint tickets with NO ESTIMATE: {ticketNoEstimate}");
+        Console.WriteLine($"     - Number of sprint tickets with NO ESTIMATE: {ticketNoEstimate} {zeroEstimateTickets}");
         if (p1Bugs > 0 || p2Bugs > 0)
         {
             Console.WriteLine($"     - *** P1 Bugs: {p1Bugs}, P2 Bugs: {p2Bugs} ***");
