de8b4fec03b073873fdd26c227c21f3617bc20d5
Ben Rees <ben@rees.biz>
2026-02-18 10:04:09 +1300

    Add extra fields

--- BensEngineeringMetrics.Console/Tasks/RemainingWorkEnvestTask.cs
+++ BensEngineeringMetrics.Console/Tasks/RemainingWorkEnvestTask.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using BensEngineeringMetrics.Jira;
 
 namespace BensEngineeringMetrics.Tasks;
@@ -6,13 +7,16 @@ namespace BensEngineeringMetrics.Tasks;
 ///     This task retrieves all Jira Issues related to top-level Jira Initiatives for the customer Envest
 ///     that are marked as "Required For Go-live" but are not yet Done.
 /// </summary>
-public class RemainingWorkEnvestTask(IJiraIssueRepository jiraIssueRepository, IOutputter outputter, IWorkSheetUpdater sheetUpdater) : IEngineeringMetricsTask
+public class RemainingWorkEnvestTask(IJiraIssueRepository jiraIssueRepository, IOutputter outputter, IWorkSheetUpdater sheetUpdater, IJiraQueryRunner runner) : IEngineeringMetricsTask
 {
     private const string TaskKey = "REMAINING_WORK_ENVEST";
     private const string GoogleSheetId = "1OVUx08nBaD8uH-klNAzAtxFSKTOvAAk5Vnm11ALN0Zo";
     private const string SheetTabName = "Remaining Work";
 
-    public IList<EnvestNotDoneIssue> NotDoneIssues { get; private set; } = new List<EnvestNotDoneIssue>();
+    private static readonly IFieldMapping[] Fields = [JiraFields.StoryPoints, JiraFields.Priority, JiraFields.Exalate];
+
+    private IList<EnvestNotDoneIssue> notDoneIssues = new List<EnvestNotDoneIssue>();
+    private IList<ExtraData> extraData = new List<ExtraData>();
 
     public string Description => $"Find all {Constants.Envest} Jira Issues Required For Go-live that are not Done";
 
@@ -24,19 +28,13 @@ public class RemainingWorkEnvestTask(IJiraIssueRepository jiraIssueRepository, I
 
         await LoadData();
 
-        outputter.WriteLine($"\nFound {NotDoneIssues.Count} issues that are Required For Go-live but not Done.");
+        outputter.WriteLine($"\nFound {this.notDoneIssues.Count} issues that are Required For Go-live but not Done.");
 
         await UpdateSheet();
     }
 
     private async Task LoadData()
     {
-        if (NotDoneIssues.Any())
-        {
-            // Data already loaded
-            return;
-        }
-
         outputter.WriteLine($"Retrieving {Constants.Envest} initiatives and their issues...");
 
         await jiraIssueRepository.GetInitiatives();
@@ -82,43 +80,60 @@ public class RemainingWorkEnvestTask(IJiraIssueRepository jiraIssueRepository, I
                             basicTicket.Summary,
                             basicTicket.Status,
                             basicTicket.IssueType
-                        ));
+                            ));
                     }
                 }
             }
         }
 
-        NotDoneIssues = notDoneIssues;
+        await LoadExtraFields(notDoneIssues);
+        this.notDoneIssues = notDoneIssues;
         outputter.WriteLine($"Complete. Found {notDoneIssues.Count} issues not Done.");
+    }
+
+    private async Task LoadExtraFields(IList<EnvestNotDoneIssue> issues)
+    {
+        const int batchSize = 1000;
+        var idArray = issues.Select(i => i.IssueKey).Distinct().ToArray();
+        var allExtraData = new List<ExtraData>();
+        for(var batchIndex = 0; batchIndex < issues.Count; batchIndex += batchSize)
+        {
+            var thisBatch = string.Join(',', idArray.Skip(batchIndex).Take(batchSize).ToArray());
+            var jql = $"key IN ({thisBatch})";
+            allExtraData.AddRange((await runner.SearchJiraIssuesWithJqlAsync(jql, Fields)).Select(ExtraData.Create));
+        }
+
+        this.extraData = allExtraData;
     }
 
     private async Task UpdateSheet()
     {
         await sheetUpdater.Open(GoogleSheetId);
-        sheetUpdater.ClearRange(SheetTabName);
-        await sheetUpdater.ClearRangeFormatting(SheetTabName, 0, 10000, 0, 26);
+        sheetUpdater.ClearRange(SheetTabName, "A2:Z10000");
+        await sheetUpdater.ClearRangeFormatting(SheetTabName, 1, 10000, 0, 26);
 
         var reportData = new List<IList<object?>>();
         var previousHeader = string.Empty;
-        var row = 0;
-        foreach (var issue in NotDoneIssues)
+        var row = 1;
+        foreach (var issue in this.notDoneIssues)
         {
             var header = issue.InitiativeKey;
             if (previousHeader != header)
             {
                 reportData.Add([JiraUtil.HyperlinkDiscoTicket(header), issue.InitiativeSummary]);
-                row++;
                 previousHeader = header;
-                await sheetUpdater.BoldCellsFormat(SheetTabName, row, row + 1, 0, 2);
+                await sheetUpdater.BoldCellsFormat(SheetTabName, row, row + 1, 0, 3);
+                row++;
             }
 
-            reportData.Add([string.Empty, JiraUtil.HyperlinkTicket(issue.IssueKey), issue.Summary, issue.Status]);
+            var extraDataRow = this.extraData.First(d => d.Key == issue.IssueKey);
+            reportData.Add([string.Empty, JiraUtil.HyperlinkTicket(issue.IssueKey), issue.Summary, issue.Status, extraDataRow.StoryPoints, extraDataRow.Priority, extraDataRow.Exalate]);
             row++;
         }
 
-        sheetUpdater.EditSheet($"{SheetTabName}!A1", reportData, true);
+        sheetUpdater.EditSheet($"{SheetTabName}!A2", reportData, true);
 
-        outputter.WriteLine($"Updated Google Sheet with {NotDoneIssues.Count} issues: https://docs.google.com/spreadsheets/d/{GoogleSheetId}/edit#gid=0");
+        outputter.WriteLine($"Updated Google Sheet with {this.notDoneIssues.Count} issues: https://docs.google.com/spreadsheets/d/{GoogleSheetId}/edit#gid=0");
         await sheetUpdater.SubmitBatch();
     }
 
@@ -133,4 +148,12 @@ public class RemainingWorkEnvestTask(IJiraIssueRepository jiraIssueRepository, I
         string Summary,
         string Status,
         string IssueType);
+
+    public record ExtraData(string Key, double StoryPoints, string Priority, string Exalate)
+    {
+        public static ExtraData Create(dynamic d)
+        {
+            return new ExtraData(JiraFields.Key.Parse(d), JiraFields.StoryPoints.Parse(d), JiraFields.Priority.Parse(d), JiraFields.Exalate.Parse(d));
+        }
+    }
 }
