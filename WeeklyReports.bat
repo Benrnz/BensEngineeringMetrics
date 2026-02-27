86e977d2cf9dde6d9f54a65f302383d1f2627461
Ben Rees <ben@rees.biz>
2026-02-27 14:15:44 +1300

    Update Daily run to include NZB stats

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -5,7 +5,7 @@ git checkout master
 git pull
 dotnet build BensEngineeringMetrics.sln
 cd BensEngineeringMetrics.Console\Bin\Debug\net9.0
-BensEngineeringMetrics.Console.exe BATCH BUG_STATS INCIDENTS INIT_ALL SPRINT_PLAN ENG_TASK_ANALYSIS ENVEST_EXALATE REMAINING_WORK_ENVEST
+BensEngineeringMetrics.Console.exe BATCH BUG_STATS INCIDENTS INIT_ALL SPRINT_PLAN ENG_TASK_ANALYSIS ENVEST_EXALATE REMAINING_WORK_ENVEST BUG_STATS_NZB
 
 ECHO Finish time: %date% %time%
 
