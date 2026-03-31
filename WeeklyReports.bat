cd64c1435ff8ad6fb5dcb8d3dc60d4e57f913b1c
Ben Rees <ben@rees.biz>
2026-04-01 12:16:39 +1300

    Update to .net10

--- WeeklyReports.bat
+++ WeeklyReports.bat
@@ -4,7 +4,7 @@ ECHO Start time: %date% %time%
 git checkout master
 git pull
 dotnet build BensEngineeringMetrics.sln
-cd BensEngineeringMetrics.Console\Bin\Debug\net9.0
+cd BensEngineeringMetrics.Console\Bin\Debug\net10.0
 BensEngineeringMetrics.Console.exe BATCH BUG_STATS INCIDENTS INIT_ALL SPRINT_PLAN ENG_TASK_ANALYSIS ENVEST_EXALATE REMAINING_WORK_ENVEST BUG_STATS_NZB DAILY
 
 ECHO Finish time: %date% %time%
